e187410f749bda5ac83f6acdc18f977d2bc3af21
vjimcor955 <vjimcor955@g.educaand.es>
2024-02-21 10:02:13 +0100

    Albums table

--- database/migrations/2024_02_21_082212_create_albums_table.php
+++ database/migrations/2024_02_21_082212_create_albums_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('albums', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('artist');
+            $table->blob('img');
             $table->timestamps();
         });
     }
