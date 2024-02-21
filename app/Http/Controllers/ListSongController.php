e1cb7c6be78c5eca5b32618e6a2077dc58d9286f
vjimcor955 <vjimcor955@g.educaand.es>
2024-02-21 09:32:37 +0100

    artisan ListSong

---
+++ app/Http/Controllers/ListSongController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\List_song;
+use App\Http\Requests\StoreList_songRequest;
+use App\Http\Requests\UpdateList_songRequest;
+
+class ListSongController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreList_songRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(List_song $list_song)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(List_song $list_song)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateList_songRequest $request, List_song $list_song)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(List_song $list_song)
+    {
+        //
+    }
+}
