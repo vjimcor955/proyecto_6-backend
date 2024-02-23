6f548873ea03713ce628b110159e1b82c0afe91b
vjimcor955 <vjimcor955@g.educaand.es>
2024-02-23 14:02:30 +0100

    songs and albums routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SongController;
+use App\Http\Controllers\AlbumController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/songs', [SongController::class, 'index']);
+Route::get('/songs/{song}', [SongController::class, 'show']);
+Route::post('/songs', [SongController::class, 'store']);
+// Route::put('/songs/{song}', [SongController::class, 'update']);
+Route::delete('/songs/{song}', [SongController::class, 'destroy']);
+
+
+Route::get('/albums', [AlbumController::class, 'index']);
+Route::get('/albums/{album}', [AlbumController::class, 'show']);
+Route::post('/albums', [AlbumController::class, 'store']);
+// Route::put('/songs/{song}', [AlbumController::class, 'update']);
+Route::delete('/albums/{album}', [AlbumController::class, 'destroy']);
